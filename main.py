5156d52fbb162c5fa6456a0af612f08db3712a4f
Jimuelle07 <patronjimuelle26@gmail.com>
2026-04-04 01:58:42 +0800

    added files

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import requests  
+import requests 
 import API
 
-r = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API.weather_api_key}")
+base_url = "https://pokeapi.co/api/v2/"
 
-r.json()
-print(r)
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+
+    else:
+        print(f"Failed to retrieve data. {response.status_code}")
+
+pokemon_name = "mudkip"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    print(f"{pokemon_info["name"]}")
+    print(f"{pokemon_info["id"]}")
